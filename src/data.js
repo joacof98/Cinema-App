87394d5a91666a9bb35ad79b01aeb7a056a27aa4
joaco <joacofracchia@gmail.com>
2020-12-08 11:17:27 -0300

    added imgur imgs

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const movies = [
         id: 1,
         title: "Avengers Endgame",
         desc: "Los Vengadores restantes deben encontrar una manera de recuperar a sus aliados para un enfrentamiento épico con Thanos, el malvado que diezmó el planeta y el universo.",
-        img: "https://i.etsystatic.com/15963200/r/il/b10de2/1833903984/il_570xN.1833903984_yjpb.jpg",
+        img: "https://i.imgur.com/ER3HCDk.jpg",
         priceChildren: 110,
         priceAdult: 200,
         year: 2019
@@ -12,7 +12,7 @@ export const movies = [
         id: 2,
         title: "Parasite",
         desc: "Tanto Gi Taek como su familia están sin trabajo. Cuando su hijo mayor, Gi Woo, empieza a recibir clases particulares en la adinerada casa de Park, las dos familias, que tienen mucho en común pese a pertenecer a dos mundos totalmente distintos, comienzan una relación de resultados imprevisibles.",
-        img: "https://images-na.ssl-images-amazon.com/images/I/81T219mv5iL._AC_SX522_.jpg",
+        img: "https://i.imgur.com/MP6v0n4.jpg",
         priceChildren: 90,
         priceAdult: 180,
         year: 2019
@@ -22,7 +22,7 @@ export const movies = [
         id: 3,
         title: "Midsommar",
         desc: "Una pareja de estadounidenses acude con unos amigos a Midsommar, un festival de verano que se celebra cada 90 años en una aldea remota de Suecia. Sin embargo, lo que parecían ser unas vacaciones de ensueño toma un giro macabro cuando los aldeanos los invitan a participar en sus perturbadoras actividades festivas.",
-        img: "https://images-na.ssl-images-amazon.com/images/I/71WkWl-YLjL._SY741_.jpg",
+        img: "https://i.imgur.com/2X1SZR4.jpg",
         priceChildren: 100,
         priceAdult: 200,
         year: 2019
@@ -31,7 +31,7 @@ export const movies = [
         id: 4,
         title: "1917",
         desc: "Durante la guerra mundial, dos jóvenes soldados británicos reciben ordenes aparentemente imposibles. En una carrera a contrarreloj, deben infiltrarse en territorio enemigo para entregar un mensaje que podría salvar miles de vidas.",
-        img: "https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_.jpg",
+        img: "https://i.imgur.com/CcGLKyq.jpg",
         priceChildren: 130,
         priceAdult: 210,
         year: 2020
@@ -40,7 +40,7 @@ export const movies = [
         id: 5,
         title: "Knives Out",
         desc: "Benoit Blanc, un detective implacable, investiga la muerte de un anciano escritor de novelas policíacas en la mansión del difunto. Benoit tendrá que sortear las trampas y mentiras que la excéntrica familia y los sirvientes del novelista han urdido.",
-        img: "https://m.media-amazon.com/images/M/MV5BMGUwZjliMTAtNzAxZi00MWNiLWE2NzgtZGUxMGQxZjhhNDRiXkEyXkFqcGdeQXVyNjU1NzU3MzE@._V1_.jpg",
+        img: "https://i.imgur.com/koc2BDx.jpg",
         priceChildren: 130,
         priceAdult: 210,
         year: 2019
@@ -49,7 +49,7 @@ export const movies = [
         id: 6,
         title: "Spideman: Spiderverse",
         desc: "Luego de ser mordido por una araña radioactiva, el joven Miles Morales desarrolla misteriosos poderes que lo transforman en el Hombre Araña. Ahora deberá usar sus nuevas habilidades ante el malvado Kingpin, un enorme demente que puede abrir portales hacia otros universos.",
-        img: "https://images-na.ssl-images-amazon.com/images/I/91Tr%2BbhnMUL._AC_SL1500_.jpg",
+        img: "https://i.imgur.com/yyPV9gt.jpg",
         priceChildren: 80,
         priceAdult: 160,
         year: 2018
@@ -58,7 +58,7 @@ export const movies = [
         id: 7,
         title: "Un lugar tranquilo 2",
         desc: "La familia Abbott ahora se enfrenta a los terrores del mundo exterior. Forzados a aventurarse en lo desconocido, se dan cuenta de que las criaturas que cazan por el sonido no son las únicas amenazas que acechan más allá del camino de arena.",
-        img: "https://m.media-amazon.com/images/M/MV5BN2NiMjEyZjAtNzYyYi00YjI3LThhZTYtMzkyZTkxYzgwMzI2XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+        img: "https://i.imgur.com/iiC1Jn7.jpg",
         priceChildren: 120,
         priceAdult: 200,
         year: 2020
@@ -67,11 +67,11 @@ export const movies = [
         id: 8,
         title: "Star Wars Episodio IX",
         desc: "La Resistencia sobreviviente se enfrenta a la Primera Orden, y Rey, Finn, Poe y el resto de los héroes encararán nuevos retos y una batalla final con la sabiduría de las generaciones anteriores.",
-        img: "https://cuatrocirculos.com/wp-content/uploads/2020/01/star-wars.jpeg",
+        img: "https://i.imgur.com/Ez1clI0.jpg",
         priceChildren: 70,
         priceAdult: 150,
         year: 2019
     }
 ];
 
-export const imgMini = 'https://i7.pngguru.com/preview/1022/338/397/movie-icons-film-clapperboard-cinema-icon-movie-clapper-cliparts.jpg';
+export const imgMini = 'https://i.imgur.com/VLZTxSD.jpg';
